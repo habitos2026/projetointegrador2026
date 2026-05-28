43fcd3b229f9686bac5a37eb7a9c54f60d864e56
adrianoirano <adrianoirano747@gmail.com>
2026-05-28 11:41:41 -0300

    trabalho.js

--- trabalho.js
+++ trabalho.js
@@ -47,12 +47,16 @@ function calc() {
     // mensagem
 const msg = document.getElementById("msg"); 
 const gif = document.getElementById("gif"); 
+const imgGif = document.createElement('img');
 
 if(porcentagem === 100){ 
     msg.innerText = "Parabéns! Você completou todos os hábitos 🎉"; 
-    gif.setAttribute = "src", "pasta-de-imagens/gifParabens.gif"; 
+    imgGif.src = 'pasta-de-imagens/gifParabens.gif';
+    imgGif.alt = 'Comemoração';
+    imgGif.style.width = '100px';
+    imgGif.style.marginLeft = '10px';
 } else { 
     msg.innerText = ""; 
-    gif.setAttribute = "src", ""; 
+    container.insertAdjacentElement('afterend', imgGif);
 }
 }
