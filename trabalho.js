8d383f2a062d81bd7ec5103b438c4b24d328e26d
Gabriel Henrique <gabrielforcesx854@gmail.com>
2026-06-10 22:51:10 +0000

    Jun 10, 2026, 7:51 PM

--- trabalho.js
+++ trabalho.js
@@ -12,13 +12,13 @@ fetch("menu.html")
 
     // Botão tema
     const btnTema = document.getElementById("btnTema");
-    btnTema.textContent = document.body.classList.contains("dark") ? "Tema claro ☀️" : "Tema escuro 🌙";
+    btnTema.textContent = document.body.classList.contains("dark") ? "☀️" : "🌙";
 
     btnTema.addEventListener("click", () => {
         document.body.classList.toggle("dark");
         const temaDark = document.body.classList.contains("dark");
         localStorage.setItem("tema", temaDark ? "dark" : "light");
-        btnTema.textContent = temaDark ? "Tema claro ☀️" : "Tema escuro 🌙";
+        btnTema.textContent = temaDark ? "☀️" : "🌙";
     });
 
     // Botões do menu mobile
